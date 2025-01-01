90fe3f248b5aaaab7f75e5af3dc4e614ed9530f7
ripticno <taamir.haider@gmail.com>
2025-01-01 17:08:46 +0600

    py.vs code

---
+++ birthday.py
@@ -0,0 +1,10 @@
+taamir="2013 june 20"
+ayaz="2013 december 4"
+wasfi="2013 march 26"
+ohon="2014 septemder 12"
+suffan="2013 april 28"
+print(taamir)
+print(ayaz)
+print(ohon)
+print(wasfi)
+print(suffan)
