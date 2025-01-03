9cd68c23b76d2b990698b3da453c9f8973c12500
ripticno <taamir.haider@gmail.com>
2025-01-03 13:24:06 +0600

    we

---
+++ squer root persentage generator.py
@@ -0,0 +1,17 @@
+a=int(input("plese enter your marks of math out of 100 "))
+b=int(input("plese enter your marks of english out of 100 "))
+c=int(input("pese enter your marks of bangla out of 100 "))
+d=int(input("plese enter your marks of science out of 100 "))
+t=a**0.5
+u=b**0.5
+y=c**0.5
+r=d**0.5
+e=int(input("plse enter you total marks for everything "))
+m=a/e*100
+n=b/e*100
+v=c/e*100
+x=d/e*100
+print("the squer root persentage of the marks of math is", m)
+print("the squer root persentage of the marks of english is", n)
+print("the squer root persentage of the marks of bangla is", v)
+print("the squer root persentage of the marks of science is", x)
