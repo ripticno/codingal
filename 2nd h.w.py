fbe5b71032df405df5dbc66d397de2ecebdc2ec8
ripticno <taamir.haider@gmail.com>
2025-01-05 23:21:09 +0600

    py

---
+++ 2nd h.w.py
@@ -0,0 +1,13 @@
+unit=int(input("plese enter your unit of electry usage in the month "))
+if unit<0:
+    print("somethint went wrong try again")
+    unit=int(input("plese enter your unit of electry usage in the month"))
+elif unit>=101 and unit<300:
+    resulta=100*5+(unit-100)*8
+    print(resulta)
+elif unit<=100 and unit>1:
+    resultb=(unit*5)
+    print()
+else:
+    resultd=100*5+(unit-100)*8+(unit-300)*10
+    print("you have to pay $", resultd)
