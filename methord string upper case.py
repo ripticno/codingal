9831b7e1fc2bd93670721f57a935686e5a5e0e9b
ripticno <taamir.haider@gmail.com>
2025-06-01 21:51:47 +0600

    dfd

---
+++ methord string upper case.py
@@ -0,0 +1,9 @@
+class fruit():
+    def __init__(self,name,colour):
+        self.colour=colour
+        self.name=name
+    def sto (self):
+        print(self.name)
+        print(self.colour)
+apple=fruit("apple","blue")   
+apple.sto()         
