c31a4c6fcca5430d1ca9f4d1ebf7f7fdb3ba9d7a
ripticno <taamir.haider@gmail.com>
2025-06-02 13:36:40 +0600

    ljjid

---
+++ emloy 101.py
@@ -0,0 +1,18 @@
+ampout_of_work2=int(input("enter the days you worked"))
+skipp_work2=int(input("enter the days of work you skiped"))
+class employ():
+    def __init__(self,ampout_of_work,skipp_work):
+        self.ampout_of_work=ampout_of_work
+        self.skipp_wprk=skipp_work
+    def cal(self):
+        cal=ampout_of_work2<skipp_work2
+        cal2=ampout_of_work2>skipp_work2
+        if cal:
+            print("this employ has not worked all the days so he is termanited")
+        elif  :
+            print("this employ is hard working keep it up")
+
+dis=employ()
+dis.cal
+# miss this is not working help me
+        
