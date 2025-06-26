51a08bfb3e636ba2279e692a5500ef267ae9d82b
ripticno <taamir.haider@gmail.com>
2025-06-26 11:43:45 +0600

    fjwfjwefkwpwokqpdskwghapf'k

--- g store.py
+++ g store.py
@@ -1,8 +1,13 @@
-user=str(input("enter which product you want "))
-def drinks(price,vat):
-    price=5
-    vat=2
-    total=vat+price
-    return total
-print(drinks(5,2)
+needed=int(input("welcome to g store what do you want"))
+vat=2
+def drink(vat,prize):
+    prize=5
+    grand_totle=(vat+prize)
+    return grand_totle
+def chips(vat,prize):
+    prize=3
+    grand_totle=(vat+prize)
+    return grand_totle
+    
+    
       
