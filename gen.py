20baae46ecf2acdac9ee6f70dac6bb0eb00ea2f1
ripticno <taamir.haider@gmail.com>
2025-07-03 13:40:28 +0600

    KEEP IT PRIVET

--- gen.py
+++ gen.py
@@ -1,18 +1,18 @@
 class dad:
-    def  _init_(self,eye,aggressive):
+    def  __init__(self,eye,aggressive):
         self.eye=eye
         self.aggressive=aggressive
     def display(self):
         print(self.eye)
         print(self.aggressive)
 class son(dad):
-    def _init_(self,eye,aggressive,age,skin):
+    def __init__(self,eye,aggressive,age,skin):
         self.age=age
         self.skin=skin
-        dad._init_(self,eye,aggressive)
+        dad.__init__(self,eye,aggressive)
     def display2(self):
         print(self.age)
         print(self.skin)
-odj=son("black",True,19,"brown")
-obj.display()
-obj.display2()
+object=son("black",True,19,"brown")
+object.display()
+object.display2()
---
+++ keep it privet.py
@@ -0,0 +1,8 @@
+class student:
+    __student20="pass"
+    def __init__(self):
+        print("the student pass")
+    def __privet(self):
+        print("the student fail")
+obj=student()
+obj.__student20()
