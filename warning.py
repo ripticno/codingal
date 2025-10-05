00ce33ebd847852d8f186cb708f8c087aa63d78c
ripticno <taamir.haider@gmail.com>
2025-10-05 13:47:44 +0600

    Create warning.py

---
+++ warning.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import messagebox
+root=Tk()
+root.geometry("200x200")
+def msg():
+    messagebox.showwarning("Alert","stop! virus Found.")
+button=Button(root,text="scan for virus",command=msg)
+button.place(x=40,y=80)
+root.mainloop()
+
